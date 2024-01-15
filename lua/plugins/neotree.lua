b837d4984ea8b572c10b1c69fe27d0d449a5316b
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-01-14 21:04:24 -0500

    added document symbols

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,19 +2,31 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
     },
     config = function()
         vim.keymap.set('n', '<leader>n', ':Neotree show filesystem toggle right<CR>')
         vim.keymap.set('n', '<leader>m', ':Neotree filesystem toggle float<CR>')
 
         require('neo-tree').setup({
+            sources = {
+                "filesystem",
+                "buffers",
+                "git_status",
+                "document_symbols",
+            },
             source_selector = {
                 winbar = true,
-                statusline = false
-            }
+                statusline = false,
+                sources = {
+                    { source = 'filesystem' },
+                    { source = 'buffers' },
+                    { source = 'git_status' },
+                    { source = 'document_symbols' },
+                }
+            },
         })
     end
 }
