f7ee24a1190001111a9e65f184bcca77421b96a1
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-01-14 21:05:38 -0500

    added mini {pairs, animate, align}

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    config = function()
+      require('mini.pairs').setup()
+    end
+  },
+
+  {
+    'echasnovski/mini.animate',
+    version = false,
+    config = function()
+      require('mini.animate').setup()
+    end
+  },
+
+  {
+    'echasnovski/mini.align',
+    version = false,
+    config = function()
+      require('mini.align').setup()
+    end
+  }
+}
