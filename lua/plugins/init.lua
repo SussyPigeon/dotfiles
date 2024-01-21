3f48ba0388d90e76c81776d4a048176b04d009ec
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-01-20 22:35:18 -0500

    added rustaceanvim plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -208,4 +208,10 @@ return {
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^3', -- Recommended
+    ft = { 'rust' },
+  },
 }
