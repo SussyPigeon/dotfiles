c0f79c74986411451814f08575a66cadd105ae12
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-09-05 13:42:37 -0400

    added file explorer to the homepage

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -10,6 +10,7 @@ return {
         dashboard.section.buttons.val = {
             dashboard.button('n', '  New file', ':ene <BAR> startinsert <CR>'),
             dashboard.button('r', '  Recent files', ':Telescope oldfiles <CR>'),
+            dashboard.button('m', '󰈢  Explore files', ':Neotree filesystem toggle float<CR>'),
             dashboard.button('f', '󰥨  Find file', ':Telescope find_files <CR>'),
             dashboard.button('g', '󰱼  Find text', ':Telescope live_grep <CR>'),
             dashboard.button('l', '󰒲  Lazy', ':Lazy<CR>'),
