45fb9da18bf9daa64e3d42c18b60d52db722d6b1
SussyPigeon <112900122+SussyPigeon@users.noreply.github.com>
2024-09-06 11:52:15 -0400

    Update setup.sh
    packages like lazygit takes more steps without Homebrew

--- setup.sh
+++ setup.sh
@@ -5,20 +5,13 @@ PACKAGES=("neovim" "lazygit" "eza")
 
 install_packages() {
     echo "Installing packages . . ."
-
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        sudo apt update
-        sudo apt install -y "${PACKAGES[@]}"
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        if ! command -v brew &> /dev/null; then
-            echo "Homebrew not found. Installing . . ."
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        fi
-        brew install "${PACKAGES[@]}"
-    else
-        echo "Unsupported OS."
-        exit 1
+    
+    if ! command -v brew &> /dev/null; then
+        echo "Homebrew not found. Installing . . ."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
+    brew update
+    brew install "${PACKAGES[@]}"
 }
 
 ###
