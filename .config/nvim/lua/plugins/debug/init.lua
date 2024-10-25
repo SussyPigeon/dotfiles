4dbfc75223b51c1a1ad68f8c3e8444396cebf98a
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-10-25 19:17:02 -0400

    removed old nvim config

--- .config/nvim/lua/plugins/debug/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-    require("plugins.debug.rust"),
-    require("plugins.debug.debug"),
-}
--- .config/nvim/lua/plugins/reticle.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'tummetott/reticle.nvim',
-    event = 'VeryLazy',
-    config = function()
-        local reticle = require('reticle')
-        reticle.setup()
-        reticle.set_cursorline(true)
-    end
-}
