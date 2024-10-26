ba14aa9a5570940b1ee1805a510c2bdeba492c23
SussyPigeon <andy.lin.4@stonybrook.edu>
2024-10-25 22:23:14 -0400

    added wsl clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,4 +9,21 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.smarttab = true
 
+-- Line number settings
 vim.opt.relativenumber = false
+
+-- Clipboard settings
+if vim.fn.had("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r",""))',
+    },
+    cache_enabled = 0,
+  }
+end
